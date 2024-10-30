bdf3ac9150f837b6f9ad2596155d4d54ec2f7707
Manuhegde1309 <manuhegde1309@gmail.com>
2024-10-30 09:33:39 +0530

    added values to db

--- db.sql
+++ db.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 9.0.1, for macos14 (arm64)
 --
--- Host: localhost    Database: dbms_project_testing1
+-- Host: localhost    Database: dbms_project_testing
 -- ------------------------------------------------------
--- Server version	8.0.39
+-- Server version	9.0.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -38,7 +38,7 @@ CREATE TABLE `Admin` (
 
 LOCK TABLES `Admin` WRITE;
 /*!40000 ALTER TABLE `Admin` DISABLE KEYS */;
-INSERT INTO `Admin` VALUES ('admf31','Mohammed','Furqaan','Patel','$argon2id$v=19$m=65536,t=3,p=4$cppClCyw38RoMUIar2T7Fw$o1wXr9mc/ZwX7xzAL7xvr+rimkhkPbdX3loMDhtDbiI'),('admh13','Manu','Narayan','Hegde','$argon2id$v=19$m=65536,t=3,p=4$exaj8cuUaDAc7DpNr2XnLw$ToZXUg4cDU46D9aQn9eu7HJ4VFL1g4PrjOMzf1sV0s8');
+INSERT INTO `Admin` VALUES ('a001','john','','doe','$argon2id$v=19$m=65536,t=3,p=4$rYAQFyeSzbMZd0PzHEW0Vw$Ikd8sUVipTKodVCBl1FbaEQWiFDCMJ3uzA0hHRdsg/c'),('a002','manu','','hegde','$argon2id$v=19$m=65536,t=3,p=4$worgQoRoq03r/vjtoqqIFA$4GjwEYGNq7nd5+9orr3qDY+Ufol9+LWQbuoi/WSYDQI'),('a003','jane','mary','doe','$argon2id$v=19$m=65536,t=3,p=4$tKbN8J0+R+IeOAlthdG1Rg$313zLWFyg9d7KzE6i9OKtBT0Qqg+wyJ9wbalbeOY1Pc'),('a004','alice','','johnson','$argon2id$v=19$m=65536,t=3,p=4$qPeAf1khWv4vOYJOGHIaMw$GY9UQ/vHOZ4lmqIb+unDgcbN7S+6m9mCt8PSrqb0zow'),('a005','mohammed','','furqaan','$argon2id$v=19$m=65536,t=3,p=4$FKbr1M/4SLbAMD1CpPlheQ$ClbwJF/Isks8SALErIegkbMoSh1O5tKhoFPy8VuclE4'),('a006','bob','','brown','$argon2id$v=19$m=65536,t=3,p=4$bXlA4Wsht4MnZFFF4TMh5w$AdbhMvP1aa4nFrXuxwJnnAYIXFXfkdJ4ihJH+wDiopg');
 /*!40000 ALTER TABLE `Admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +64,6 @@ CREATE TABLE `Company_Inventory` (
 
 LOCK TABLES `Company_Inventory` WRITE;
 /*!40000 ALTER TABLE `Company_Inventory` DISABLE KEYS */;
-INSERT INTO `Company_Inventory` VALUES ('ccpg1','CEMENT',90),('ccpg1','IRON',35180),('ccah1','IRON',50070),('ccah1','NAIL',60010),('ccpg1','NAIL',100);
 /*!40000 ALTER TABLE `Company_Inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -93,7 +92,7 @@ CREATE TABLE `Construction_Company` (
 
 LOCK TABLES `Construction_Company` WRITE;
 /*!40000 ALTER TABLE `Construction_Company` DISABLE KEYS */;
-INSERT INTO `Construction_Company` VALUES ('ccah1','Ashed',999488450.00,'admh13'),('ccpg1','Prestige',2730500.00,'admf31');
+INSERT INTO `Construction_Company` VALUES ('c001','buildcorp',10000000.00,'a001'),('c002','ABC Constructions',10000000.00,'a002'),('c003','XYZ Builders',10000000.00,'a003'),('c004','summit builders',10000000.00,'a004'),('c005','peaky blinders',10000000.00,'a005'),('c006','beta builders',10000000.00,'a006');
 /*!40000 ALTER TABLE `Construction_Company` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,9 +130,44 @@ CREATE TABLE `Order_info` (
 
 LOCK TABLES `Order_info` WRITE;
 /*!40000 ALTER TABLE `Order_info` DISABLE KEYS */;
-INSERT INTO `Order_info` VALUES ('OR0133','fe35','ccpg1','sca2b1','DHL',20000,3000000.00,'Accepted'),('OR07e1','cmt1','ccah1','sca2b1',NULL,50,100000.00,'Rejected'),('OR256f','fe35','ccah1','sca2b1','FDX',60,9000.00,'Accepted'),('OR2666','fe35','ccpg1','sca2b1','FDX',10,1500.00,'Accepted'),('OR5087','cmt1','ccpg1','sca2b1',NULL,50,100000.00,'Rejected'),('OR665b','nl1','ccah1','scpes1','FDX',60000,3000000.00,'Accepted'),('OR6af6','nl1','ccah1','scpes1','FDX',2,100.00,'Accepted'),('OR8530','cmt2','ccpg1','scpes1','DHL',50,1250000.00,'Accepted'),('OR8708','cmt1','ccpg1','sca2b1','FDX',10,20000.00,'Accepted'),('OR943d','fe45','ccpg1','scpes1','DHL',80,8000.00,'Accepted'),('OR976c','nl1','ccah1','scpes1','DHL',9,450.00,'Accepted'),('ORae28','fe35','ccpg1','sca2b1','DHL',15000,2250000.00,'Accepted'),('ORbd1f','fe45','ccpg1','scpes1','DHL',25,2500.00,'Accepted'),('ORc141','fe35','ccpg1','sca2b1','FDX',10,1500.00,'Accepted'),('ORc446','cmt2','ccpg1','scpes1','DHL',40,1000000.00,'Accepted'),('ORc7b1','fe45','ccpg1','scpes1','FDX',100,10000.00,'Accepted'),('ORc8a1','nl1','ccpg1','scpes1','DHL',100,5000.00,'Accepted'),('ORcab8','fe45','ccah1','scpes1','FDX',20,2000.00,'Accepted'),('ORe143','nl1','ccpg1','scpes1','DHL',50,2500.00,'Accepted'),('OReb2e','fe35','ccah1','sca2b1','DHL',50000,7500000.00,'Accepted'),('ORfc76','fe35','ccpg1','sca2b1','FDX',25,3750.00,'Accepted');
 /*!40000 ALTER TABLE `Order_info` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `update_stock_on_order_accept` AFTER UPDATE ON `order_info` FOR EACH ROW BEGIN
+            DECLARE current_stock INT;
+
+            IF NEW.Status = 'Accepted' THEN
+                -- Check current stock
+                SELECT Stock INTO current_stock
+                FROM Product_info
+                WHERE Product_id = NEW.Product_id;
+
+                -- Restock if necessary
+                IF current_stock < NEW.Quantity THEN
+                    UPDATE Product_info
+                    SET Stock = current_stock + (NEW.Quantity * 2)
+                    WHERE Product_id = NEW.Product_id;
+                END IF;
+
+                -- Deduct the ordered quantity from stock
+                UPDATE Product_info
+                SET Stock = Stock - NEW.Quantity
+                WHERE Product_id = NEW.Product_id;
+            END IF;
+        END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `Product_info`
@@ -160,7 +194,7 @@ CREATE TABLE `Product_info` (
 
 LOCK TABLES `Product_info` WRITE;
 /*!40000 ALTER TABLE `Product_info` DISABLE KEYS */;
-INSERT INTO `Product_info` VALUES ('cmt1','CEMENT',1000.00,'sca2b1',10000),('cmt2','CEMENT',1500.00,'scpes1',9950),('fe35','IRON',150.00,'sca2b1',14980),('fe45','IRON',100.00,'scpes1',9800);
+INSERT INTO `Product_info` VALUES ('p001','CEMENT',500.00,'s001',10000),('p002','BRICKS',250.00,'s001',10000),('p003','IRON',300.00,'s002',10000),('p004','GOLD',1000.00,'s002',10000),('p005','BRONZE',100.00,'s003',10000),('p006','SILVER',400.00,'s003',10000),('p007','COPPER',150.00,'s004',10000),('p008','STEEL',350.00,'s004',10000);
 /*!40000 ALTER TABLE `Product_info` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -185,7 +219,7 @@ CREATE TABLE `Shipment_Company` (
 
 LOCK TABLES `Shipment_Company` WRITE;
 /*!40000 ALTER TABLE `Shipment_Company` DISABLE KEYS */;
-INSERT INTO `Shipment_Company` VALUES ('DHL','DHL Group'),('FDX','FedEx Corp');
+INSERT INTO `Shipment_Company` VALUES ('DHL','DHL Group'),('EMS','Express Mail Service'),('FDX','FedEx Corp'),('TNT','TNT Express'),('UPS','United Parcel Service');
 /*!40000 ALTER TABLE `Shipment_Company` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -211,7 +245,7 @@ CREATE TABLE `Supplier_Company` (
 
 LOCK TABLES `Supplier_Company` WRITE;
 /*!40000 ALTER TABLE `Supplier_Company` DISABLE KEYS */;
-INSERT INTO `Supplier_Company` VALUES ('sca2b1','a2b','$argon2id$v=19$m=65536,t=3,p=4$gF0Ryats9UHatJhUcam6dw$2FsyhNqcUZshKPjRyM6NEFZFeN0HirEyHO3m9CFPqBY'),('scpes1','pes','$argon2id$v=19$m=65536,t=3,p=4$aMmI20T58dPexueJHjJOQw$UnI7jRhuzW+8nf4AUEVuoSiiXXqEww0gICizsAVrtqI');
+INSERT INTO `Supplier_Company` VALUES ('s001','Alpha Supplies','$argon2id$v=19$m=65536,t=3,p=4$YaYhWNR/TK6gXKN7BlQeZQ$A3OLkpsd34pmXdyvkds57KaF5dIIi8NGNFrbsycnC34'),('s002','Beta Suppliers','$argon2id$v=19$m=65536,t=3,p=4$s+9XWSI0uWu3lfJeJfCMmg$H+bw1rv45+HrVaCRLdslaEK9xtxZ7rJ4hkP0W7O3AC4'),('s003','Gamma Suppliers','$argon2id$v=19$m=65536,t=3,p=4$ZR2vsC3YBOf2ZZdhq6OY6w$FYAiLMD262alXnUbTnt+qV8iZpUVIbMjXfL0IY3J9iI'),('s004','Epsilon Suppliers','$argon2id$v=19$m=65536,t=3,p=4$JaySU/C8McO1M6R0B2iDow$DUawvxL7eMW8oEDug27ZnOYcBsremlckIC8MbH7bSlQ'),('s005','Delta Suppliers','$argon2id$v=19$m=65536,t=3,p=4$BPVr3FEVxaHMdZEeeaI0Iw$N6ObVtJWL7DRa4oeTfGbZCOfvJ5PwM1gHdfitBuu+JM');
 /*!40000 ALTER TABLE `Supplier_Company` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -224,4 +258,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-28 22:08:32
+-- Dump completed on 2024-10-30  9:29:49
